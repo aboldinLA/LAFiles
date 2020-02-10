2ea9494d3097ea9c60bb0c7adcbf698c348fd3ed
Mrudul Shah <mrudul.ce@gmail.com>
2020-02-10 15:00:40 +0530

    Added unique slug to articles.

--- modules/article.php
+++ modules/article.php
@@ -5,6 +5,8 @@ include './db.php';
 	$sql = "select id,title from editorial"; 
 	$result = $conn->query($sql);	 
     $i =0;
+    $duplicateCount = 0;
+
     while($row = mysqli_fetch_array($result)) {
 
 	    $string =  $row['title']; // Trim String
@@ -19,13 +21,25 @@ include './db.php';
 
 		$string = preg_replace("/[\s_]/", "-", $string);
 
-		    $sql = "UPDATE editorial SET slug='".preg_replace('/\s+/', ' ', $string)."' WHERE id=" . $row['id'] . "";
 
-	        if ($conn->query($sql) === TRUE) {
-	            echo $string."</br>";
-	        } else {
-	            echo "Error updating record: " . $conn->error;
-	        } 
+		$check = "SELECT id,slug FROM editorial WHERE slug = '".$string."'";
+		$resultCheck = $conn->query($check);
+		$checkCount = mysqli_num_rows($resultCheck);		
+		
+		if($checkCount > 0){
+			
+			$resultCheck -> free_result();
+			$string .= $string.'-'.$row['id'];
+			$duplicateCount++;
+		}
+		
+	    $sql = "UPDATE editorial SET slug='".preg_replace('/\s+/', ' ', $string)."' WHERE id=" . $row['id'] . "";
+
+        if ($conn->query($sql) === TRUE) {
+            echo $string."</br>";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        } 
     	 
     }                                       
 	 
